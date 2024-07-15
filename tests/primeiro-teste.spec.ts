fdffee8f6da385aad03f92e8d2675a075c7db1d7
lucasrafaelbf <lucas.rafael121@hotmail.com>
2024-07-15 17:30:41 -0300

    teste 2

--- tests/primeiro-teste.spec.ts
+++ tests/primeiro-teste.spec.ts
@@ -3,5 +3,8 @@ import test from "@playwright/test";
 
 test('Visitando pagina do Playwright', async ({ page }) => {
     await page.goto('https://playwright.dev');
+    //await page.locator('getStarted_Sjon').click();
+    await page.getByText('Get started').click();
+
 
 })
